e5e97ace7bd9f9b4c89631758507dc9eab75ecb7
Anna <anthateyou@gmail.com>
2024-10-13 01:19:22 +0300

    Изменена генерация данных пользователя, удалены ненужные зависимости

--- src/main/java/user/Generator.java
+++ src/main/java/user/Generator.java
@@ -1,36 +1,20 @@
 package user;
-import org.apache.commons.lang3.RandomStringUtils;
-
+import com.github.javafaker.Faker;
 
 public class Generator {
-    private static final String DOMAIN = "@gmail.com";
 
-    public static String generateUserName() {
-        return RandomStringUtils.randomAlphabetic(8);
-    }
-
-    public static String generateUserEmail() {
-        return RandomStringUtils.randomAlphabetic(8) + DOMAIN;
-    }
-
-    public static String generateUserPassword() {
-        return RandomStringUtils.randomAlphabetic(8);
-    }
-
-    public static String generateWrongUserPassword() {
-        return RandomStringUtils.randomAlphabetic(5);
-    }
+    static Faker faker = new Faker();
 
     public static User generateUser() {
-        String name = generateUserName();
-        String email = generateUserEmail();
-        String password = generateUserPassword();
+        String name = faker.name().firstName();
+        String email = faker.internet().emailAddress();
+        String password = faker.internet().password(6,7);
         return new User(name, email, password);
     }
     public static User generateUserWithWrongPass() {
-        String name = generateUserName();
-        String email = generateUserEmail();
-        String password = generateWrongUserPassword();
+        String name = faker.name().firstName();
+        String email = faker.internet().emailAddress();
+        String password = faker.internet().password(5,5);
         return new User(name, email, password);
     }
 }
